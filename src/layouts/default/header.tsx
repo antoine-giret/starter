313a0d57a716216aa5e4380ee584aa36dcdc70da
Antoine Giret <antoine.g@my-serious-game.com>
2019-10-08 20:15:38 +0200

    chore: add lingui for i18n

--- src/layouts/default/header.tsx
+++ src/layouts/default/header.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Trans } from '@lingui/react'
 
 function Header() {
-  return <></>
+  return (
+    <>
+      <h1>
+        <Trans id="app_title" />
+      </h1>
+    </>
+  )
 }
 
 export default Header
